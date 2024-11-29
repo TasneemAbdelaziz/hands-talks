6131834cb52692a6ee4bb642deb6112c7b1a560f
Tasneem AbdElaziz <97146993+TasneemAbdelaziz@users.noreply.github.com>
2024-11-29 20:42:16 +0200

    theming class commit

---
+++ lib/theming.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Theming{
+  static Color primary = const Color(0xff00081D);
+  // used in buttons
+  static Color secondary = const Color(0xffD9D9D9);
+
+  // used in filed like email ,password,text inside gray buttons..
+  static Color form = const Color(0xff858C94);
+
+  // snakBar when filled and stroke when no filled
+  static Color snakBar = const Color(0xff808187);
+
+
+}
